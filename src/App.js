50f9adb0624d2b63c9db9353937f39f72e433ac5
Jack Gill <jackgill@trustedhousesitters.com>
2018-12-12 13:03:00 +0000

    Completes an ugly version of the list view.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.getResultsDebounced = debounce(this.getResults, 250, true);
+        this.getResultsDebounced = debounce(this.getResults, 100);
 
         this.state = {
             searchTerm: '',
             searching: false,
             searchDisabled: false,
+            searchResults: [],
         };
     }
 
@@ -39,7 +40,7 @@ class App extends Component {
                 .then((response) => {
                     // handle success
                     console.log(response);
-                    this.setState({searching: false})
+                    this.handleSearchResultData(response);
                 })
                 .catch(function (error) {
                     // handle error
@@ -50,52 +51,70 @@ class App extends Component {
                 });
     };
 
+    handleSearchResultData = (searchData) => {
+        const hits = (searchData.data && searchData.data.response) ? searchData.data.response.hits : [];
+
+        const newResults = hits.map((item) => {
+            const { result } = item;
+            return {
+                name: result.title,
+                artist: result.primary_artist ? result.primary_artist.name : 'unknown',
+                thumbnailURL: result.header_image_thumbnail_url,
+                hot: result.stats ? result.stats.hot : false,
+            };
+        });
+
+        this.setState({
+            searching: false,
+            searchResults: newResults,
+        });
+    };
+
     render() {
-        const { searching } = this.state;
+        const {
+            searching,
+            searchTerm,
+            searchResults
+        } = this.state;
+        console.log(searchResults.length);
         return (
             <Wrapper>
-                <form
-                    onSubmit={this.handleSubmit}
+                <div
                     style={{
                         display: 'flex',
                         flexDirection: 'column',
-                        alignItems: 'center',
-                        maxWidth: '320',
                     }}
                 >
-                    <input
-                        type="text"
-                        value={this.state.searchTerm}
-                        onChange={this.handleChange}
-                        disabled={this.state.searchDisabled}
+                    <form
+                        onSubmit={this.handleSubmit}
                         style={{
-                            borderStyle: 'solid',
-                            borderWidth: '1px',
-                            fontSize: '2rem',
-                            backgroundColor: this.state.searchDisabled ? 'grey' : 'white',
+                            maxWidth: '320',
                         }}
-                    />
-                </form>
-                <ResultsScrollView>
-                    {!searching && songData.map((item) => (
-                        <ResultsItem {...item} />
-                    ))}
-                </ResultsScrollView>
+                    >
+                        <input
+                            type="text"
+                            value={this.state.searchTerm}
+                            onChange={this.handleChange}
+                            disabled={this.state.searchDisabled}
+                            style={{
+                                borderStyle: 'solid',
+                                borderWidth: '1px',
+                                fontSize: '2rem',
+                                backgroundColor: this.state.searchDisabled ? 'grey' : 'white',
+                            }}
+                        />
+                    </form>
+                    {searchResults.length > 0 && searchTerm.length > 0 &&
+                        <ResultsScrollView>
+                            {searchResults.map((item) => (
+                                <ResultsItem  key={`results-item-${item.name}-${item.artist}`} {...item} />
+                            ))}
+                        </ResultsScrollView>}
+                </div>
             </Wrapper>
         );
     }
 }
-
-const songData = [
-    {
-        name: 'The Song',
-        artist: 'The Artist',
-    },
-    {
-        name: 'The Other Song',
-        artist: 'The Other Artist',
-    },
-];
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
