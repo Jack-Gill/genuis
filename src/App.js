36c056091b3be82ab86b790269f923f5d20f94bd
Jack Gill <jackgill@trustedhousesitters.com>
2018-12-12 10:35:12 +0000

    Gets rid of yarn lock and sets up basic call and response for input/search.

--- src/App.js
+++ src/App.js
@@ -4,37 +4,54 @@ import ResultsScrollView from './components/ResultsScrollView';
 import axios from 'axios';
 import ResultsItem from './components/ResultsItem';
 
-const BASE_URL = '/.netlify/functions/proxy/'
+const BASE_URL = '/.netlify/functions/proxy/';
 
 class App extends Component {
-    state = {
-        searchTerm: '',
-        searchDisabled: false,
-    };
+    constructor(props) {
+        super(props);
+
+        this.getResultsDebounced = debounce(this.getResults, 250, true);
+
+        this.state = {
+            searchTerm: '',
+            searching: false,
+            searchDisabled: false,
+        };
+    }
 
     handleChange = (event) => {
-        this.setState({searchTerm: event.target.value});
+        const searchTerm = event.target.value;
+        this.setState({
+            searchTerm,
+            searching: true,
+        });
+        this.getResultsDebounced(searchTerm)
     };
 
     handleSubmit = (event) => {
         this.setState({searchDisabled: true});
         event.preventDefault();
+        this.getResults(this.state.searchTerm);
+    };
 
-        axios.get(`${BASE_URL}search?q=${this.state.searchTerm}`)
-            .then(function (response) {
-                // handle success
-                console.log(response);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+    getResults = (searchTerm) => {
+        axios.get(`${BASE_URL}search?q=${searchTerm}`)
+                .then((response) => {
+                    // handle success
+                    console.log(response);
+                    this.setState({searching: false})
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
     };
 
     render() {
+        const { searching } = this.state;
         return (
             <Wrapper>
                 <form
@@ -60,7 +77,7 @@ class App extends Component {
                     />
                 </form>
                 <ResultsScrollView>
-                    {songData.map((item) => (
+                    {!searching && songData.map((item) => (
                         <ResultsItem {...item} />
                     ))}
                 </ResultsScrollView>
@@ -79,5 +96,24 @@ const songData = [
         artist: 'The Other Artist',
     },
 ];
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
 export default App;
