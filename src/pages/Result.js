66d1fc568f4d1214eaae0dc10a19ee4599c47535
Chris Guy <christien.guy@gmail.com>
2019-01-09 10:28:40 +0000

    feat(Result & scrape.js): Add scraper to get genius lyrics. Add Result page to display them.

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,17 +1,36 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
 import { BASE_URL } from "constants.js";
+import styled from "styled-components";
+
+const Referent = styled.a`
+    display: inline;
+    background-color: #c9d4ff;
+    box-shadow: 0.01em 0 0 #c9d4ff, -0.01em 0 0 #c9d4ff;
+    padding: 3px;
+    line-height: 1.7;
+
+    text-decoration: none;
+    color: #222;
+
+    &:hover {
+        background-color: #acbaef;
+        box-shadow: 0.01em 0 0 #acbaef, -0.01em 0 0 #acbaef;
+    }
+`;
 
 class Result extends Component {
     state = {
         song: null,
-        lyricsHtml: null
+        lyricsData: null
     };
 
     scrapePage = path => {
         axios.get(`${BASE_URL}scrape?path=${path}`).then(({ data }) => {
+            console.log(data);
+
             this.setState({
-                lyricsHtml: data.lyricsHtml
+                lyricsData: data.lyricsData
             });
         });
     };
@@ -19,18 +38,16 @@ class Result extends Component {
     componentDidMount() {
         const { resultId } = this.props;
         axios.get(`${BASE_URL}proxy/songs/${resultId}`).then(result => {
-            console.log(result.data.response.song);
             this.setState({
                 song: result.data.response.song
             });
             this.scrapePage(result.data.response.song.path);
         });
-
     }
 
     render() {
         const { resultId } = this.props;
-        const { song, lyricsHtml } = this.state;
+        const { song, lyricsData } = this.state;
 
         return (
             <div>
@@ -40,12 +57,29 @@ class Result extends Component {
                         <div>{song.primary_artist.name}</div>
                     </div>
                 ) : null}
-                {lyricsHtml ? (
-                    <div
-                        dangerouslySetInnerHTML={{
-                            __html: lyricsHtml
-                        }}
-                    />
+                {lyricsData ? (
+                    <div>
+                        {lyricsData.map(({ text, referentId }) => {
+                            if (referentId) {
+                                return (
+                                    <Fragment>
+                                        <Referent
+                                            href={`/referent/${referentId}`}
+                                            data-referent-id={referentId}
+                                            dangerouslySetInnerHTML={{
+                                                __html: text.replace(
+                                                    /(?:\r\n|\r|\n)/g,
+                                                    "<br />"
+                                                )
+                                            }}
+                                        />
+                                        <br />
+                                    </Fragment>
+                                );
+                            }
+                            return <p>{text}</p>;
+                        })}
+                    </div>
                 ) : null}
             </div>
         );
