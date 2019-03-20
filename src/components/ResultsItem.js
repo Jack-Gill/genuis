df8d8758503a037681ad5e5e5135f23f2d6d4e4c
Jack-Gill-TH <37797332+Jack-Gill-TH@users.noreply.github.com>
2019-03-20 10:38:34 +0000

    Gh5 display search result list (#7)

--- src/components/ResultsItem.js
+++ src/components/ResultsItem.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ResultsItem = ({name, artist}) => (
-    <Wrapper>
-        <div>
-            {name}
-        </div>
-        <div>
-            {artist}
-        </div>
-    </Wrapper>
+const ResultsItem = ({name, artist, thumbnailURL, hot, onClick}) => (
+    <div onClick={onClick}>
+        <Wrapper>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+                <img src={thumbnailURL} alt={"Smiley face"} height={"42"} width={"42"}/>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '0 5px'
+            }}>
+                <span>{name}</span>
+                <span style={{
+                    fontWeight: '300',
+                }}>{artist}</span>
+            </div>
+            {hot &&
+                <div style={{
+                    marginLeft: 'auto',
+                    paddingRight: '8px',
+                }}>
+                    <i className="material-icons">hot_tub</i>
+                </div>
+            }
+        </Wrapper>
+    </div>
 );
 
 const Wrapper = styled.div`
     width: 320px;
-    border: 1px solid red;
+    border: 1px solid grey;
     display: flex;
     flex-direction: row;
     align-items: center;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,70 +2,153 @@ import React, { Component } from "react";
 import axios from "axios";
 // components
 import ResultsScrollView from "components/ResultsScrollView";
+import ResultsItem from "components/ResultsItem";
 import Wrapper from "components/Wrapper";
 
 import { BASE_URL } from 'constants.js';
 
 class Home extends Component {
-    state = {
-        searchTerm: "",
-        searchDisabled: false
+    constructor(props) {
+        super(props);
+
+        this.getResultsDebounced = debounce(this.getResults, 100);
+
+        this.state = {
+            searchTerm: '',
+            searching: false,
+            viewingItem: false,
+            searchDisabled: false,
+            searchResults: [],
+        };
+    }
+
+    handleChange = (event) => {
+        const searchTerm = event.target.value;
+        this.setState({
+            searchTerm,
+            searching: true,
+        });
+        this.getResultsDebounced(searchTerm)
     };
 
-    handleChange = event => {
-        this.setState({ searchTerm: event.target.value });
-    };
-
-    handleSubmit = event => {
-        this.setState({ searchDisabled: true });
+    resultItemClicked = (event) => {
         event.preventDefault();
+        this.setState({
+            viewingItem: true,
+            searchDisabled: true,
+        });
+        console.log('No');
+    };
 
-        axios
-            .get(`${BASE_URL}/proxy/search?q=${this.state.searchTerm}`)
-            .then(function(response) {
+    handleSubmit = (event) => {
+        // this.setState({searchDisabled: true});
+        // event.preventDefault();
+        // this.getResults(this.state.searchTerm);
+    };
+
+    getResults = (searchTerm) => {
+        axios.get(`${BASE_URL}proxy/search?q=${searchTerm}`)
+            .then((response) => {
                 // handle success
                 console.log(response);
+                this.handleSearchResultData(response);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 // handle error
                 console.log(error);
             })
-            .then(function() {
+            .then(function () {
                 // always executed
             });
     };
 
+    handleSearchResultData = (searchData) => {
+        const hits = (searchData.data && searchData.data.response) ? searchData.data.response.hits : [];
+
+        const newResults = hits.map((item) => {
+            const { result } = item;
+            return {
+                name: result.title,
+                artist: result.primary_artist ? result.primary_artist.name : 'unknown',
+                thumbnailURL: result.header_image_thumbnail_url,
+                hot: result.stats ? result.stats.hot : false,
+            };
+        });
+
+        this.setState({
+            searching: false,
+            searchResults: newResults,
+        });
+    };
+
     render() {
+        const {
+            searching,
+            searchTerm,
+            searchResults,
+            viewingItem,
+        } = this.state;
+        console.log(searchResults.length);
         return (
             <Wrapper>
-                <form
-                    onSubmit={this.handleSubmit}
+                <div
                     style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        maxWidth: "320"
+                        display: 'flex',
+                        flexDirection: 'column',
                     }}
                 >
-                    <input
-                        type="text"
-                        value={this.state.searchTerm}
-                        onChange={this.handleChange}
-                        disabled={this.state.searchDisabled}
+                    <form
+                        onSubmit={this.handleSubmit}
                         style={{
-                            borderStyle: "solid",
-                            borderWidth: "1px",
-                            fontSize: "2rem",
-                            backgroundColor: this.state.searchDisabled
-                                ? "grey"
-                                : "white"
+                            maxWidth: '320',
                         }}
-                    />
-                </form>
-                <ResultsScrollView />
+                    >
+                        <input
+                            type="text"
+                            value={this.state.searchTerm}
+                            onChange={this.handleChange}
+                            disabled={this.state.searchDisabled}
+                            style={{
+                                borderStyle: 'solid',
+                                borderWidth: '1px',
+                                fontSize: '2rem',
+                                backgroundColor: this.state.searchDisabled ? 'grey' : 'white',
+                            }}
+                        />
+                    </form>
+                    {!viewingItem && searchResults.length > 0 && searchTerm.length > 0 &&
+                    <ResultsScrollView>
+                        {searchResults.map((item) => (
+                            <ResultsItem
+                                key={`results-item-${item.name}-${item.artist}`}
+                                onClick={this.resultItemClicked}
+                                {...item}
+                            />
+                        ))}
+                    </ResultsScrollView>}
+                </div>
             </Wrapper>
         );
     }
 }
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
 export default Home;
