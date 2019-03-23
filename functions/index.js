78ae829dc0cf505a4aca7ec37e379a9b219e6935
Chris Guy <christien.guy@gmail.com>
2019-03-23 23:49:55 +0000

    Add cors management to functions

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,15 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 const { Translate } = require("@google-cloud/translate");
 
 // require'd local modules
 const { proxyRequest } = require("./proxy");
+
+const corsHandler = cors({
+    origin: ["localhost:3000"]
+});
 
 // variable initialization
 const TOO_MANY_SEGMENTS_MESSAGE = "Too many text segments";
@@ -69,17 +74,17 @@ async function translateSongLyrics(lyricsData) {
             let translationResults = await translate.translate(
                 lyricsText,
                 firstTarget
-                );
-                // Result of a translate call is always an array, the first item of which is the translated text/texts
-                let translatedLyrics = translationResults[0];
-                
+            );
+            // Result of a translate call is always an array, the first item of which is the translated text/texts
+            let translatedLyrics = translationResults[0];
+
             // eslint-disable-next-line
             translationResults = await translate.translate(
                 translatedLyrics,
                 secondTarget
-                );
-                translatedLyrics = translationResults[0];
-                
+            );
+            translatedLyrics = translationResults[0];
+
             // eslint-disable-next-line
             translatedLyrics.forEach((result, index) => {
                 lyricsDataCopy[index].text = result;
@@ -99,100 +104,106 @@ async function translateSongLyrics(lyricsData) {
     return lyricsDataCopy;
 }
 
-exports.getWarpedSong = functions.https.onRequest(async (request, res) => {
-    const { songId } = request.query;
-    // attempt to get songId out of firebase
-    const songDocRef = db.collection("songs").doc(songId);
+exports.getWarpedSong = functions.https.onRequest(async (req, res) => {
+    corsHandler(req, res, async () => {
+        const { songId } = req.query;
+        // attempt to get songId out of firebase
+        const songDocRef = db.collection("songs").doc(songId);
 
-    console.log("GET WARPED SONG");
+        console.log("GET WARPED SONG");
 
-    let responseObj = {};
+        let responseObj = {};
 
-    songDocRef
-        .get()
-        .then(async doc => {
-            console.log("RESOLVED REQUEST FOR DOC");
+        songDocRef
+            .get()
+            .then(async doc => {
+                console.log("RESOLVED REQUEST FOR DOC");
 
-            // if song with matching id exists
-            if (doc.exists) {
-                console.log("DOC EXISTS");
+                // if song with matching id exists
+                if (doc.exists) {
+                    console.log("DOC EXISTS");
 
-                // if warped version exists.
-                const { original, warped, name } = doc.data();
-                if (warped) {
-                    console.log("HAS WARPED");
-                    // return it
-                    res.send({ name, warped });
-                    return;
-                } else if (original) {
+                    // if warped version exists.
+                    const { original, warped, name } = doc.data();
+                    if (warped) {
+                        console.log("HAS WARPED");
+                        // return it
+                        res.send({ name, warped });
+                        return;
+                    } else if (original) {
+                        // translate to warped version
+                        const warped = await translateSongLyrics(original);
+                        // store in firebase
+                        songDocRef.set({
+                            name,
+                            original,
+                            warped
+                        });
+                        // return it
+                        res.send({
+                            name,
+                            warped
+                        });
+                        return;
+                    }
+                } else {
+                    console.log("DOC DOES NOT EXIST");
+
+                    // else
+                    // scrape it to get original
+                    const { data } = await proxyRequest(`/songs/${songId}`, {});
+
+                    const song = data.response.song;
+                    console.log("GOT DATA");
+
+                    const path = song.path;
+                    const originalLyrics = await scrapeContent(path);
+                    console.log("ORIGINAL LYRICS");
+
                     // translate to warped version
-                    const warped = await translateSongLyrics(original);
+                    const warpedLyrics = await translateSongLyrics(
+                        originalLyrics
+                    );
+                    console.log("WARPED LYRICS", warpedLyrics);
+
                     // store in firebase
-                    songDocRef.set({
-                        name,
-                        original,
-                        warped
-                    });
+                    try {
+                        songDocRef.set({
+                            name: song.primary_artist.name,
+                            original: originalLyrics,
+                            warped: warpedLyrics
+                        });
+                    } catch (err) {
+                        console.log(err);
+                    }
                     // return it
                     res.send({
-                        name,
-                        warped
+                        name: song.primary_artist.name,
+                        warped: warpedLyrics
                     });
                     return;
                 }
-            } else {
-                console.log("DOC DOES NOT EXIST");
-
-                // else
-                // scrape it to get original
-                const { data } = await proxyRequest(`/songs/${songId}`, {});
-
-                const song = data.response.song;
-                console.log("GOT DATA");
-
-                const path = song.path;
-                const originalLyrics = await scrapeContent(path);
-                console.log("ORIGINAL LYRICS");
-
-                // translate to warped version
-                const warpedLyrics = await translateSongLyrics(originalLyrics);
-                console.log("WARPED LYRICS", warpedLyrics);
-
-                // store in firebase
-                try {
-                    songDocRef.set({
-                        name: song.primary_artist.name,
-                        original: originalLyrics,
-                        warped: warpedLyrics
-                    });
-                } catch (err) {
-                    console.log(err);
-                }
-                // return it
-                res.send({
-                    name: song.primary_artist.name,
-                    warped: warpedLyrics
-                });
                 return;
-            }
-            return;
-        })
-        .catch(err => {
-            res.send(err);
-        });
+            })
+            .catch(err => {
+                res.send(err);
+            });
+    });
 });
 
-const proxy = functions.https.onRequest(async (request, res) => {
-    const { params, originalUrl } = request;
-    //TODO: get correct endpoint
+const proxy = functions.https.onRequest(async (req, res) => {
+    corsHandler(req, res, async () => {
+        const { params, originalUrl } = request;
+        //TODO: get correct endpoint
 
-    try {
-        const response = await proxyRequest(originalUrl, params);
-        res.send(response.data);
-    } catch (error) {
-        console.error("ERROR", error);
-        res.send(error.message);
-    }
+        try {
+            const response = await proxyRequest(originalUrl, params);
+            res.send(response.data);
+        } catch (error) {
+            console.error("ERROR", error);
+            res.send(error.message);
+        }
+    });
 });
 
 exports.proxy = proxy;
